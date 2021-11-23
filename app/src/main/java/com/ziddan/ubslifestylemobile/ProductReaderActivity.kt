7074d38ae22e897c94773a9ff0340d9fb1accdbe
muhziddan <ziddan.mz@gmail.com>
2021-11-23 19:38:37 +0700

    Update

---
+++ app/src/main/java/com/ziddan/ubslifestylemobile/ProductReaderActivity.kt
@@ -0,0 +1,11 @@
+package com.ziddan.ubslifestylemobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductReaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_reader)
+    }
+}
