e03d75899bbc9f257fd19fc80e761440c128bb02
muhziddan <ziddan.mz@gmail.com>
2021-11-23 19:39:08 +0700

    Update

--- app/src/main/java/com/ziddan/ubslifestylemobile/DetailProductActivity.kt
+++ app/src/main/java/com/ziddan/ubslifestylemobile/DetailProductActivity.kt
@@ -1,18 +1,16 @@
 package com.ziddan.ubslifestylemobile
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.ziddan.ubslifestylemobile.databinding.ActivityDetailProductBinding
 
 class DetailProductActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityDetailProductBinding
+
+    companion object {
+        const val EXTRA_COURSE = "extra_course"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,19 +20,6 @@ class DetailProductActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_detail_product)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_detail_product)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
